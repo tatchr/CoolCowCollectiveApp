59e21221368e3f7ffe7fb7103cc653d9763f345a
Home <Home@DESKTOP-Q1E658I>
2019-06-17 22:34:46 +0200

    - Back button exits app from root page.
     #15 - Exit app with back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from './services/authService/auth.service';
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -30,14 +31,25 @@ export class AppComponent {
       this.splashScreen.hide();
 
       this.authService.authenticationState.subscribe(state => {
-        if(state){
+        if (state) {
           this.storage.get('userId').then(userId => {
-            this.router.navigate(['farm-overview/' + userId], {replaceUrl: true});
-          });          
-        } else{
+            this.subscribeBackButton('/farm-overview/' + userId);
+            this.router.navigate(['farm-overview/' + userId], { replaceUrl: true });
+          });
+        }
+        else {
+          this.subscribeBackButton('/login');
           this.router.navigate(['login']);
         }
       });
+    });
+  }
+
+  subscribeBackButton(routeName) {
+    this.platform.backButton.subscribe(async () => {
+      if (this.router.isActive(routeName, true) && this.router.url === routeName) {
+        navigator['app'].exitApp();
+      }
     });
   }
 }
